bacb519ae1f75784209db0d5c879c8778c7610cd
Bhagat Mashaly <mashaly100200@hotmail.com>
2018-08-04 13:04:08 +0200

    Update ionic2-rating.module.ts

--- src/ionic2-rating.module.ts
+++ src/ionic2-rating.module.ts
@@ -2,7 +2,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from 'ionic-angular';
 
-import { Ionic2Rating } from './ionic2-rating';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
