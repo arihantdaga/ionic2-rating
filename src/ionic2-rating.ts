b2862d4f1e8c9d421ce7ed481aaa659405110ab7
Arihant <arihant.ekism@gmail.com>
2021-12-25 19:39:53 +0530

    fix: import Provider

--- src/ionic2-rating.ts
+++ src/ionic2-rating.ts
@@ -1,4 +1,4 @@
-import {Component, forwardRef, Input, ViewEncapsulation} from '@angular/core';
+import {Component, forwardRef, Input, Provider, ViewEncapsulation} from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 const noop = () => {};
